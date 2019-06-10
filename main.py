e6dcb50b50e18d1412ba058fecb9020110373c3c
lauraannf <lauraanng@gmail.com>
2019-06-10 16:54:05 -0400

    mailroom

--- main.py
+++ main.py
@@ -1,23 +1,35 @@
 import os
 import base64
-
 from flask import Flask, render_template, request, redirect, url_for, session
+from model import Donation, Donor
 
-from model import Donation 
 
 app = Flask(__name__)
+
 
 @app.route('/')
 def home():
     return redirect(url_for('all'))
 
+
 @app.route('/donations/')
 def all():
     donations = Donation.select()
     return render_template('donations.jinja2', donations=donations)
-    
+
+
+@app.route('/create', methods=['GET', 'POST'])
+def create():
+    if request.method == 'POST':
+        donor = Donor(name=request.form['name'])
+        donor.save()
+        donation = Donation(value=request.form['donation'])
+        donation.save()
+        return redirect(url_for('all'))
+    else:
+        return render_template('create.jinja2')
+
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 6738))
     app.run(host='0.0.0.0', port=port)
-
